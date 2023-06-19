9c90135f88f4d6e142f5262051936bd4d6e72ace
ekataghac <ekata.ghimire@anchorpoints.com.np>
2023-06-19 11:13:28 +0545

    Clock ticking corrected

--- 2_JS_Clock/style.js
+++ 2_JS_Clock/style.js
@@ -1,11 +1,24 @@
 const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 //1.create a function setDate
 function setDate() {
     // console.log('hi'); //3.This is to check whether it is giving hi everysecond or not 
     const now = new Date();
+
     const seconds = now.getSeconds();
-    const secondsDegres = ((seconds / 60) * 360);  //4. making the seconds in the degree 
-    console.log(seconds);
+    const secondsDegres = ((seconds / 60) * 360) + 90;  //4. making the seconds in the degree 
+    secondHand.style.transform = `rotate(${secondsDegres}deg)`;
+    // console.log(seconds);
+
+    const mins = now.getMinutes();
+    const minsDegree = ((mins / 60) * 360) + 90;
+    minHand.style.transform = `rotate(${minsDegree}deg)`;
+
+    const hour = now.getHours();
+    const hoursDegree = ((hour / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegree}deg)`;
+
 }
 
 //2.run something everysecond - we use the setInterval 
